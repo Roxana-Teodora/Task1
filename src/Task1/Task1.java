442b0336bbfc46821f0fc574b2c42dcea80af5ef
Roxana-Teodora <roxana-teodora.cibotaru@student.tuiasi.ro>
2022-05-20 01:13:25 +0300

    A doua ramura

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,11 +1,18 @@
 package Task1;
 
 public class Task1 {
-
+	
 	public static void main(String[] args) {
+		int p [] = {1, 5, 20};
+		
+		for (int i=0; i< p.length; i++)
+		{
+			System.out.println("--");
+		}
 		System.out.println("Primul commit");
 		System.out.println("Al doilea commit");
 		System.out.println("Buna");
+		
 	}
 
 }
