7c99c07ddf51055ff1f74dfb0435af7f0d0a0905
Roxana-Teodora <roxana-teodora.cibotaru@student.tuiasi.ro>
2022-05-20 00:23:58 +0300

    Al doilea commit

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -5,6 +5,7 @@ public class Task1 {
 	public static void main(String[] args) {
 		System.out.println("Primul commit");
 		System.out.println("Al doilea commit");
+		System.out.println("Buna");
 	}
 
 }
